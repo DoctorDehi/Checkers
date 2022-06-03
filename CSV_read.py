ef24a1ab10a37f0dae15881c8fdb61d8342515a0
Yorrii <hegerit@seznam.cz>
2022-06-03 10:20:03 +0200

    try, except
    obalení načítání do try

--- CSV_read.py
+++ CSV_read.py
@@ -8,19 +8,20 @@ class Hrač:
         self.barva = barva
 
     def nacti_figurku(self, b: Board):
-        with open('nacti.csv', 'r') as soubor:
-            reader = csv.reader(soubor)
-            for row in reader:
-                if row[1] == "b" and self.barva == Color.BLACK:
-                    self.figurky.append(Man(Color.BLACK, Position.from_notation(row[0]), b))
-                elif row[1] == "bb" and self.barva == Color.BLACK:
-                    self.figurky.append(King(Color.BLACK, Position.from_notation(row[0]), b))
-                elif row[1] == "w" and self.barva == Color.WHITE:
-                    self.figurky.append(Man(Color.WHITE, Position.from_notation(row[0]), b))
-                elif row[1] == "ww" and self.barva == Color.WHITE:
-                    self.figurky.append(King(Color.WHITE, Position.from_notation(row[0]), b))
-                else:
-                    print("Něco je špatně")
+        try:
+            with open('nacti.csv', 'r') as soubor:
+                reader = csv.reader(soubor)
+                for row in reader:
+                    if row[1] == "b" and self.barva == Color.BLACK:
+                        self.figurky.append(Man(Color.BLACK, Position.from_notation(row[0]), b))
+                    elif row[1] == "bb" and self.barva == Color.BLACK:
+                        self.figurky.append(King(Color.BLACK, Position.from_notation(row[0]), b))
+                    elif row[1] == "w" and self.barva == Color.WHITE:
+                        self.figurky.append(Man(Color.WHITE, Position.from_notation(row[0]), b))
+                    elif row[1] == "ww" and self.barva == Color.WHITE:
+                        self.figurky.append(King(Color.WHITE, Position.from_notation(row[0]), b))
+        except:
+            print("V načítání se vyskytla chyba")
     
     def __repr__(self):
         return self.figurky
@@ -30,6 +31,7 @@ def main():
     hrac = Hrač(Color.BLACK)
     hrac.nacti_figurku(b)
     print(*hrac.figurky)
+    b.nice_print()
 
 
 
