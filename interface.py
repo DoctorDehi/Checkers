a90eca94d0374e93abdc104558c39be24c0db8d0
doctordehi <doctordehi@gmail.com>
2022-06-21 23:42:15 +0200

    Menu:  -Načítání z CSV  -Bot move

--- interface.py
+++ interface.py
@@ -1,5 +1,4 @@
 from kivy.app import App
-from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -10,7 +9,7 @@ from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 
 from enum import Enum
 
-from pieces import Man, King, Position
+from pieces import Man, King
 
 
 class SquareColor(Enum):
@@ -54,9 +53,9 @@ class Square(Button):
                     Color(0.45, 0.45, 0.45)
 
                     Line(pos=(self.x + self.width // 6, self.y + self.height // 6),
-                        ellipse=(
-                        self.x + self.width // 3, self.y + self.height // 3, self.width // 3, self.height // 3),
-                        width=1.5)
+                         ellipse=(
+                             self.x + self.width // 3, self.y + self.height // 3, self.width // 3, self.height // 3),
+                         width=1.5)
 
             if self.selectable:
                 Color(0, 1, 0)
@@ -99,6 +98,8 @@ class BoardWidget(GridLayout):
 
     def square_clicked(self, square):
         player = self.game.current_player
+
+        # if player hasn't started the move yet and clicked on piece
         if square.piece != "None" and not self.jumped_positions:
             piece = self.game.board.get_field(square.row, square.column)
             if piece.color == player.color:
@@ -106,6 +107,7 @@ class BoardWidget(GridLayout):
                 self.next_positions = player.get_next_positions(piece, self.jumped_positions)
                 self.current_piece = piece
 
+        # else if player clicked on blank field and already selected his piece
         elif self.current_piece:
             for position in self.next_positions:
                 if square.row == position.row and square.column == position.column:
@@ -113,21 +115,37 @@ class BoardWidget(GridLayout):
                     self.jumped_positions.append(position)
                     player.find_current_valid_moves(self.current_piece, self.jumped_positions)
                     self.next_positions = player.get_next_positions(self.current_piece, self.jumped_positions)
-                    # move is done
+                    # if move is done
                     if not self.next_positions:
-                        move = player.get_move_from_positions(self.current_piece, self.jumped_positions)
-                        self.game.end_move(self.current_piece, move)
-                        self.jumped_positions.clear()
-                        self.game.toggle_player()
-                        if not self.game.current_player.valid_moves:
-                            self.game_over(winner=player)
-                        else:
-                            self.info.toggle_player()
+                        self.end_move(player)
+
                     break
         self.draw_board()
 
-    def game_over(self, winner):
-        self.info.game_over(winner)
+    def end_move(self, player):
+        move = player.get_move_from_positions(self.current_piece, self.jumped_positions)
+        self.game.end_move(self.current_piece, move)
+        self.jumped_positions.clear()
+        self.next_move()
+
+    def next_move(self):
+        self.game.toggle_player()
+        self.info.toggle_player()
+        if self.game.current_player.is_bot:
+            self.bot_move()
+        # if toggled player has no moves
+        if not self.game.current_player.valid_moves:
+            self.game_over()
+
+    def bot_move(self):
+        player = self.game.current_player
+        move = player.get_random_move()
+        self.game.make_move(move)
+        self.next_move()
+
+    def game_over(self):
+        self.parent.parent.screen_transition_victor()
+        # self.info.game_over(winner)
 
     def draw_board(self):
         board_size = self.game.board.get_size()
@@ -163,10 +181,10 @@ class InfoWidget(GridLayout):
         super().__init__(cols=2)
         self.add_widget(Label(text="Current player: "))
         self.player_label = Label(text="White" if self.player else "Black")
-        self.winner_label = Label(text="")
+        # self.winner_label = Label(text="")
         self.player = True
         self.add_widget(self.player_label)
-        self.add_widget(self.winner_label)
+        # self.add_widget(self.winner_label)
         self.bind(player=self.player_changed)
         self.game = game
 
@@ -179,23 +197,19 @@ class InfoWidget(GridLayout):
         self.player_label.color = (1, 1, 1) if self.player else (0.5, 0.5, 0.5)
 
     def game_over(self, winner):
-        self.winner_label.text = "Player  " + self.player_label.text + " won!"
-
-
-class ScreenManager(ScreenManager):
-    def __init__(self, **kwargs):
-        super(ScreenManager, self).__init__(**kwargs)
+        # self.winner_label.text = "Player  " + self.player_label.text + " won!"
+        ...
 
 
 class MenuScreen(Screen):
-    def __init__(self, game,**kwargs):
+    def __init__(self, game, **kwargs):
         super(MenuScreen, self).__init__(**kwargs)
         self.game = game
-        self.button1 = Button(text= "New Game", font_size = 32)
-        self.button2 = Button(text= "Load Game From CSV", font_size = 32)
-        self.button1.bind(on_release = self.screen_transition_button1)
-        self.button2.bind(on_release = self.screen_transition_button2)
-        self.layout = GridLayout(cols = 1)
+        self.button1 = Button(text="New Game", font_size=32)
+        self.button2 = Button(text="Load Game From CSV", font_size=32)
+        self.button1.bind(on_release=self.screen_transition_button1)
+        self.button2.bind(on_release=self.screen_transition_button2)
+        self.layout = GridLayout(cols=1)
         self.layout.add_widget(self.button1)
         self.layout.add_widget(self.button2)
         self.add_widget(self.layout)
@@ -203,21 +217,22 @@ class MenuScreen(Screen):
     def screen_transition_button1(self, *args):
         # slouží k načtení nové hry
         self.manager.current = "Menu_2"
-    
+
     def screen_transition_button2(self, *args):
         # slouží pro přechod na obrazovku hry, v případě, kdy se májí načíst pozice z CSV
+        self.game.load_game = True
         self.manager.current = "Menu_2"
 
-    
+
 class MenuScreen2(Screen):
-    def __init__(self, game,**kwargs):
+    def __init__(self, game, **kwargs):
         super(MenuScreen2, self).__init__(**kwargs)
         self.game = game
-        self.btn1 = Button(text= "Player VS Player", font_size = 32)
-        self.btn2 = Button(text= "Player VS Bot", font_size = 32)
-        self.btn1.bind(on_release = self.screen_transition_btn1)
-        self.btn2.bind(on_release = self.screen_transition_btn2)
-        self.lay = GridLayout(cols = 1)
+        self.btn1 = Button(text="Player VS Player", font_size=32)
+        self.btn2 = Button(text="Player VS Bot", font_size=32)
+        self.btn1.bind(on_release=self.screen_transition_btn1)
+        self.btn2.bind(on_release=self.screen_transition_btn2)
+        self.lay = GridLayout(cols=1)
         self.lay.add_widget(self.btn1)
         self.lay.add_widget(self.btn2)
         self.add_widget(self.lay)
@@ -226,12 +241,17 @@ class MenuScreen2(Screen):
         self.manager.current = "Checkers"
 
     def screen_transition_btn2(self, *args):
+        self.game.player_vs_bot = True
         self.manager.current = "Checkers"
 
 
 class GameScreen(Screen):
     def __init__(self, game, **kwargs):
         super(GameScreen, self).__init__(**kwargs)
+        self.board = None
+        self.info = None
+        self.surrender_btn = None
+        self.layout = None
         self.game = game
         """
         self.layout = None
@@ -250,33 +270,38 @@ class GameScreen(Screen):
 
     def on_enter(self, *args):
         self.layout = BoxLayout(orientation="horizontal")
-        self.surrender_btn = Button(text = "Surrender", font_size = 32, background_color = (0,255,255,1))
-        self.surrender_btn.bind(on_release = self.screen_transition_victor)
+        self.surrender_btn = Button(text="Surrender", font_size=32, background_color=(0, 255, 255, 1))
+        self.surrender_btn.bind(on_release=self.screen_transition_victor)
         self.info = InfoWidget(self.game)
         self.info.add_widget(self.surrender_btn)
         self.board = BoardWidget(self, cols=8, orientation="lr-bt")
-        self.board.create_board()
-        self.board.draw_board()
         self.layout.add_widget(self.board)
         self.layout.add_widget(self.info)
         self.game.current_player.find_valid_moves()
         self.add_widget(self.layout)
 
+        if self.game.load_game:
+            self.game.load_game_from_CSV("saves/nacti.csv")
+        else:
+            self.game.create_new_game()
+
+        self.board.create_board()
+        self.board.draw_board()
+
     def screen_transition_victor(self, *args):
+        self.game.game_over()
         self.manager.current = "Victor"
 
 
 class VictorScreen(Screen):
     def __init__(self, game, **kwargs):
-        super(VictorScreen,self).__init__(**kwargs)
+        super(VictorScreen, self).__init__(**kwargs)
         self.game = game
-        
+
     def on_enter(self):
-        victor = "Black" if self.game.current_player.get_color_text() == "White" else "White"
-        self.add_widget(Label(text =f"Victor is {victor}"))
-        
-        
-    
+        victor = self.game.winner.get_color_text()
+        self.add_widget(Label(text=f"Victor is {victor}"))
+
     """    
     def start_game(self, instance):
         layout = BoxLayout(orientation="horizontal")
@@ -292,11 +317,12 @@ class VictorScreen(Screen):
         return layout
     """
 
+
 class CheckersApp(App):
     def __init__(self, game):
         super().__init__()
         self.game = game
-        
+
     """
     def build(self):
         self.start = GridLayout(cols = 1)
@@ -320,11 +346,11 @@ class CheckersApp(App):
 
     def build(self):
         sm = ScreenManager(transition=FadeTransition())
-        menu = MenuScreen(self.game, name = "Menu")
-        menu2 = MenuScreen2(self.game, name = "Menu_2")
-        game = GameScreen(self.game, name = "Checkers")
-        victor = VictorScreen(self.game, name = "Victor")
-        #game.board.squares
+        menu = MenuScreen(self.game, name="Menu")
+        menu2 = MenuScreen2(self.game, name="Menu_2")
+        game = GameScreen(self.game, name="Checkers")
+        victor = VictorScreen(self.game, name="Victor")
+        # game.board.squares
         sm.add_widget(menu)
         sm.add_widget(menu2)
         sm.add_widget(game)
